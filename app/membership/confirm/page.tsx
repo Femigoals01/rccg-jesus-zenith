7adeb4735ab80e727a1ee281aef8825e6bad62ca
Osuntunde Babafemi <femigoals59@gmail.com>
2026-02-11 14:24:07 +0100

    New Update

--- app/membership/confirm/page.tsx
+++
@@ -1,8 +0,0 @@
-
-// "use client";
-
-// import MembershipConfirm from "../../../components/MemmbershipConfirm";
-
-// export default function ConfirmPage() {
-//   return <MembershipConfirm />;
-// }
